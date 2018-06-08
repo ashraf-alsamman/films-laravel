f42101f2427047772c96dcbd0157007105006faa
ashraf <ashraf.alsamman@gmail.com>
2018-06-08 16:35:08 +0300

    make migrate for database

---
+++ database/migrations/2018_06_08_130651_films.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Films extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('films', function (Blueprint $table) {
+       
+        $table->increments('id');
+        $table->string('name');
+        $table->string('description');
+        $table->date('realease_date');
+        $table->tinyInteger('rating');
+        $table->integer('ticket_price');
+        $table->integer('country_id'); 
+        $table->string('film_slug'); 
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('films');
+    }
+}
